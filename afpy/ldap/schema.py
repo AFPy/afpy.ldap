260b5ab7fe72cb3bf7aed66021e286d2c7c9dee5
Gael Pasgrimaud <gael@gawel.org>
2009-07-25 14:05:47 +0200

    test schema

--- afpy/ldap/schema.py
+++ afpy/ldap/schema.py
@@ -1,4 +1,32 @@
 # -*- coding: utf-8 -*-
+__doc__ = """You can map your :class:`~afpy.ldap.node.Node` properties::
+
+    >>> from afpy.ldap import node
+    >>> class MyUser(node.Node):
+    ...     uid = StringProperty('uid', title='login', required=True)
+    ...     birthDate = DateProperty('birthDate', title='login', required=True)
+    >>> from afpy.ldap import custom as ldap
+    >>> conn = ldap.get_conn()
+
+Get a node with this class::
+
+    >>> user = conn.get_user('gawel', node_class=MyUser)
+    >>> user
+    <MyUser at uid=gawel,ou=members,dc=afpy,dc=org>
+
+The `birthDate` is stored as string but converted to date when you access it
+via the property::
+
+    >>> user.get('birthDate')
+    '19750410000000Z'
+    >>> user.birthDate
+    datetime.date(1975, 4, 10)
+
+No validation is done at schema level. The title and required parameters are
+used for :mod:`afpy.ldap.forms` generation.
+"""
+
+
 import utils, datetime
 
 class Property(property):
